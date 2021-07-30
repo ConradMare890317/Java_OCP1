2b652f5eba0561d18d22436e74f814cb8b7180e9
ConradMare890317 <Conradmare@gmail.com>
2021-07-30 23:40:19 +0200

    2021/07/30 (pm)
    Classwork folders paired in same repo.

---
+++ Java11ClassWork/src/main/java/Chap11/arrayList.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chap11;
+
+import java.util.ArrayList;
+/**
+ *
+ * @author Conrad
+ */
+public class arrayList {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList();
+        
+        names.add("Jamie");
+        names.add(2, "Thoni");
+        
+        System.out.println(names.get(1));
+    }
+    
+}
